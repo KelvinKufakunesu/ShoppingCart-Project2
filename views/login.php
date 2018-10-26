97955586b6e330fb38298488e3ced0d52da2dcfc
lindastorgard <linda.storgard@gmail.com>
2018-10-26 15:52:36 +0200

    login fixed

--- views/login.php
+++ views/login.php
@@ -1,53 +1,40 @@
 <?php
-
 session_start();
 
-/**
-1. koppla upp till databasen
-2. hämta användaren från databasen
-3. kolla sp att lösenordet stämmer lverens med lösenordet som användaren har skrivit in i formuläre. Kolla med: password_verify
-
-*/
-
-//1. vi behöver pdo uppkoppling
+//connect to data base
 $pdo = new PDO(
 "mysql:host=localhost; dbname=FED18; charset=utf8",
     "root",
     "root"
 );
 
-//vi sparar i lösenord och användarnamn i nya variabler
-//same value in $_POST["username"] and $username
+//save as $username and $password
 $username=$_POST["username"];
 $password=$_POST["password"];
 
-
-// no whitespace between $pdo and prepare!
+//get username from database
 $statement = $pdo->prepare("SELECT * FROM users WHERE username = :username");
-
-// execute populates the statement ad runs it associative array skickas in i statement
+//execute
 $statement->execute( 
 [
     ":username" => $username
 ]
 );
 
-//2. when select is used, fetch must happen
+//fetch
 $fetched_user = $statement->fetch();
 
-var_dump($fetched_user["password"]);
+//var_dump($fetched_user["password"]);
 
-//3. compare
+//check if password is correct
 $is_password_correct = password_verify($password, $fetched_user["password"]);
+
 if($is_password_correct){
     //save user globally with session
     $_SESSION["username"] = $fetched_user["username"];
     //go back to front page
     header('Location: ../includesAndPartials/registerForm.php');
 }else{
-    //handle errors, go back to frontpage and populate $_GET
+    //handle errors, go back to register form
     header('Location: ../includesAndPartials/registerForm.php?login_failed=true');
-}
-
-
-?>
+}?>
