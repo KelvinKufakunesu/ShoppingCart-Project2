f717d35d3a5c29070be5bdad57b638d6985a08e3
lindastorgard <linda.storgard@gmail.com>
2018-10-26 09:29:19 +0200

    adds products to data base

--- views/addToChart.php
+++ views/addToChart.php
@@ -1,23 +1,57 @@
 <?php
+session_start();
 
+//data base connection
 $pdo = new PDO(
-"mysql:host=localhost; dbname=FED18; charset=utf8",
-    "root",
-    "root"
+    "mysql:host=localhost;dbname=FED18;charset=utf8",
+    "root", //user
+    "root"  //password
 );
+    
+//include product array
+//Prepare a SQL-statement
+$statement = $pdo->prepare("SELECT * FROM products");
+//Execute it
+$statement->execute();
+//Fetch every row that it returns. $products is now an Associative array
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$amount1=$_POST["amount1"];
+//print_r($products);// check that I have the associative array
 
 
-$statement = $pdo->prepare("INSERT INTO shoppingChart (amount1) VALUES(:amount1)");
+foreach($products as $product){
+    if ($_POST[$product["name"]] >= 1){
+    
+            $_SESSION[$product["name"]] = $_POST[$product["name"]];
+        
+    $name = $product["name"];
+    $price = $product["price"];
+    $amount = $_SESSION[$product["name"]];
+    $username = $_SESSION["username"];
+    
+    $statement = $pdo->prepare("INSERT INTO orders (name, price, amount, username) VALUES(:name, :price, :amount, :username)");
 
-$statement->execute([
-  ":amount1" => $amount1,
-]);
+    $statement->execute( 
+    [
+        ":name" => $name,
+        ":price" => $price,
+        ":amount" => $amount,
+        ":username" => $username
+    ]
+    );
+        
+    }    
+}
+header('Location: checkout.php');
 
 
-//header('Location: ../index.php');
 
-echo "Hej"
 
+//die();
+
+
+   
 ?>
+
+
+
