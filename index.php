5e764b7bee67fc07a9afff89f6bced7165cd3d8f
lindastorgard <linda.storgard@gmail.com>
2018-10-26 09:28:37 +0200

    fixed index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ session_start();
 
 <body>
 
-
     <!-- include Header -->
     <?php include 'includesAndPartials/header.php';?>
 
@@ -18,147 +17,64 @@ session_start();
         <div class="row">
             <section class="col-12">
                 <h1 class="welcomeHeader">Hi Apple Lover</h1>
-                <p class="welcomeText">Welcome to our appelicious web shop. Please register or sign in to start shopping!</p>
-
-                <!-- include discount text if Monday or Friday -->
-                <?php include 'includesAndPartials/indexDiscountText.php';?>
-
-                <a class="btn btn-dark registerButton" href="includesAndPartials/registerForm.php" role="button">Register or sign in</a>
-
+                <div class="welcomeText">Welcome to our appelicious web shop. Please register or sign in to start shopping!</div>
+                
                 <?php 
+                //welcome message to username
                 if(isset($_SESSION["username"])){
                     echo '<div class="orderHeading">Välkommen ' . $_SESSION["username"] . '</div>';
-                    echo '<div class="orderText">Happy shopping!</div>';
                 } 
+                
+                //if user is not signed in with username -> error message 
+                if(isset($_GET["not_registered"])){
+                    echo '<div class="orderText">Register or sign in to start shopping!</div>';
+                    }
                 ?>
+                
+                <a class="btn btn-dark registerButton" href="includesAndPartials/registerForm.php" role="button">Register or sign in here</a>
 
             </section>
         </div>
     </div>
 
-
-
     <div class="container">
         <div class="row">
-<!--            <form class="col-12" action="linda_storgard_crud/view/checkout.php" method="POST">-->
+            <form class="col-12" action="views/addToChart.php" id="formId" method="POST">
                 <div class="row">
-                
-                <!-- include product array from database -->
-                <?php include 'includesAndPartials/productArray.php';?>
-                
-                
-
-<!--
-                    <div class='form-group col-md-6 appleForm'>
-                        <img src='images/apple.jpg' alt='apple'>
--->
-<!--
-                        <label class="appleText" for='ingrid'>Ingrid Marie 2 kr</label>
-                        <input class="amountInputField" id="ingrid" name="amount" type="number" min="0">
--->
-
-<!--
-                        <form action="views/addToChart.php" method="POST">
-                            <label for="amount1"></label>
-                            <input class="btn btn-dark" type="submit" id="amount1" name="amount1" value="Add to Chart">
-                        </form>
--->
-                        
-<!--
-                    <form action="views/addToChart.php" method="POST">
-                    <label for="amount1">amount</label>
-                    <input type="text" name="amount1" id="amount1">
-                    <input type="submit" value="Register">
-                    </form>
--->
-             
-<!--
-                        <form action="views/removeFromChart.php" method="POST">
-                            <label for="amount1"></label>
-                            <input class="btn btn-dark" type="submit" id="amount1" name="amount1" value="Remove from Chart">
-                        </form>
--->
-<!--                    </div>-->
-
-<!--
-                    <div class='form-group col-md-6 appleForm'>
-                        <img src='images/apple.jpg' alt='apple'>
-                        <label class="appleText" for='honey'>Honey Crunch 3 kr</label>
-                        <input class="amountInputField" id="honey" name="amount" type="number" min="0">
-
-                        <form action="views/addToChart.php" method="POST">
-                            <label for="amount2"></label>
-                            <input class="btn btn-dark" type="submit" value="Add to Chart">
-                        </form>
-
-                        <form action="views/removeFromChart.php" method="POST">
-                            <label for="amount2"></label>
-                            <input class="btn btn-dark" type="submit" value="Remove from Chart">
-                        </form>
-                    </div>
-
-                    <div class='form-group col-md-6 appleForm'>
-                        <img src='images/apple.jpg' alt='apple'>
-                        <label class="appleText" for='rubinstar'>Rubinstar 4 kr</label>
-                        <input class="amountInputField" id="ingrid"  name="amount" type="number" min="0">
-
-                        <form action="views/addToChart.php" method="POST">
-                            <label for="amount3"></label>
-                            <input class="btn btn-dark" type="submit" value="Add to Chart">
-                        </form>
-
-                        <form action="views/removeFromChart.php" method="POST">
-                            <label for="amount3"></label>
-                            <input class="btn btn-dark" type=" submit" value="Remove from Chart">
-                        </form>
-                    </div>
-
-                    <div class='form-group col-md-6 appleForm'>
-                        <img src='images/apple.jpg' alt='apple'>
-                        <label class="appleText" for="discovery">Discovery 250 kr</label>
-                        <input class="amountInputField" id="discovery" name="amount" type="number" min="0">
-
-                        <form action="views/addToChart.php" method="POST">
-                            <label for="amount4"></label>
-                            <input class="btn btn-dark" type="submit" value="Add to Chart">
-                        </form>
-
-                        <form action="views/removeFromChart.php" method="POST">
-                            <label for="amount4"></label>
-                            <input class="btn btn-dark" type="submit" name="amount4" value="Remove from Chart">
-                        </form>
-                    </div>
--->
-
-                </div>
-
 
                 <?php
-//                    $pdo = new PDO(
-//                    "mysql:host=localhost; dbname=FED18; charset=utf8",
-//                    "root",
-//                    "root"
-//                    );
-                ?>
-
-
-<!--            </form>-->
+                //connect to data base
+                include 'includesAndPartials/databaseConnection.php';
+                    
+                include 'includesAndPartials/productArray.php';
+        
+                //Loops through products and echoes to index-page
+                foreach($products as $product){
+                    echo "<div class='form-group col-md-6 appleForm'>";
+                    echo "<img src='images/apple.jpg' alt='apple'>";
+                    echo '<label class="appleText" for=' . $product["name"] . '> ' . $product["name"] . ' ' . number_format($product["price"], 2) . ' kr/piece</label><br>';
+                    echo '<input class="amountInputField" id=' . $product["name"] . ' name=' . $product["name"] . ' type="number" min="0">';
+                    echo '<button type="submit" class="btn btn-dark">add</button>';
+                    echo "</div>";
+                    }                 
+                    ?>
+                </div>
+            </form>
         </div>
     </div>
 
     <div class="container">
         <div class="row">
             <form class="col-12">
-                <button type="submit" class="btn btn-dark checkoutButton">Go to checkout</button>
                 <a class="btn btn-dark checkoutButton" href="includesAndPartials/logout.php" role="button">Logout</a>
             </form>
         </div>
     </div>
 
     <?php
-//    if(isset($_GET["error"])) {
-//    echo ($_GET["error"]);
-//    }
+    if(isset($_GET["error"])) {
+    echo ($_GET["error"]);
+    }
     ?>
 
     <!-- include Footer -->
