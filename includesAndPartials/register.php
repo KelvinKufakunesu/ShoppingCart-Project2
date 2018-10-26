ec8fd16cb548490c3100a274308829164fa82597
lindastorgard <linda.storgard@gmail.com>
2018-10-26 15:54:23 +0200

    code done

--- includesAndPartials/register.php
+++ includesAndPartials/register.php
@@ -1,35 +1,24 @@
 <?php
-/**
-tar in lösenordet och lägger in det i databasen - redirectar till index.php för att logga in
+session_start();
 
-No HTML
-mission: spara användare i databasen
-1. handtera post variabler: $_POST
-2. koppling till databas: PDO
-3. spara användaren i databasen
-*/
-
+//connection to data base
 $pdo = new PDO(
 "mysql:host=localhost; dbname=FED18; charset=utf8",
     "root",
     "root"
 );
 
-
-//same value in $_POST["username"] and $username
+//save as $username
 $username=$_POST["username"];
 $password=$_POST["password"];
 
-//password_hash must always have two arguments
+//make hashed password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-
-//LÄGG IN HÄR!!!! -----> if lösenord är true etc etc kontrollera med if sats
-
-// no whitespace between $pdo and prepare!
+//insert username and password into database
 $statement = $pdo->prepare("INSERT INTO users(username, password) VALUES(:username, :password)");
 
-// execute populates the statement ad runs it associative array skickas in i statement
+//execute
 $statement->execute( 
 [
     ":username" => $username,
@@ -37,7 +26,6 @@ $statement->execute(
 ]
 );
 
+//redirect to register form
 header('Location: registerForm.php');
-
-
 ?>
