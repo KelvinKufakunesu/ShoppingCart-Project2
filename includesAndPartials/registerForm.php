0d36f54f500a4eb3ef3ca12640c8748ddf9ffea8
lindastorgard <linda.storgard@gmail.com>
2018-10-26 15:54:02 +0200

    page done

--- includesAndPartials/registerForm.php
+++ includesAndPartials/registerForm.php
@@ -59,7 +59,6 @@ session_start();
                     <input class="btn btn-dark" type="submit" value="Register">
                 </form>
                 
-
                 <div class="loginText">Login</div>
                 <form action="../views/login.php" method="POST">
                     <label for="login_username"></label>
@@ -69,25 +68,21 @@ session_start();
                     <input class="btn btn-dark" type="submit" value="Login">
                 </form>
 
-               
                 <?php
+                //handle failed login
                 if(isset($_GET["login_failed"])){
-                    echo "Skriv in rätt användarnamn och lösenord";
+                    echo '<div class="orderText">Skriv in rätt användarnamn och lösenord</div>';
                 }
-       
+                
+                //if login is done correctly -> show welcome message
                 if (isset($_SESSION["username"])){
-        
-                    echo 'Hej' . $_SESSION["username"];
-                }
-                ?>
-
-               
-                <a class="btn btn-dark logoutButton" href="../index.php" role="button">Start shopping</a>
-
+                    echo '<div class="orderText">Welcome ' . $_SESSION["username"] . '</div>';
+                    echo '<a class="btn btn-dark logoutButton" href="../index.php" role="button">Start shopping</a>';
+                }?>
+                
             </section>
         </main>
     </div>
-
 
     <footer>
         <p class="footerText">Shop 'til you drop</p>
@@ -98,7 +93,6 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
 
 </body>
 </html>
